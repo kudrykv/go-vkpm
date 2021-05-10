d16684b7cfd29f25150a1d07ffb0853583678171
Vitaliy Kudryk <kudryk.v@protonmail.com>
2021-05-10 16:14:44 +0300

    random test

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/antchfx/htmlquery v1.2.3
 	github.com/antchfx/xpath v1.1.10 // indirect
 	github.com/jwalton/gchalk v1.0.3
+	github.com/smartystreets/goconvey v1.6.4
 	github.com/urfave/cli/v2 v2.3.0
 	golang.org/x/net v0.0.0-20200813134508-3edf25e44fcc
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
---
+++ types/date_test.go
@@ -0,0 +1,18 @@
+package types_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/kudrykv/go-vkpm/types"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestDate_Equal(t *testing.T) {
+	Convey("Equal", t, func() {
+		p1 := types.Today()
+		p2 := types.Date{Time: p1.Round(time.Second)}
+
+		So(p1.Equal(p2), ShouldBeTrue)
+	})
+}
